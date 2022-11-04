9123ca35e5dacad43e836eece20c80b126707453
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-11-04 23:28:24 +0530

    Added firebase realtime db

--- lib/view/practice_page.dart
+++ lib/view/practice_page.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:rehab/utils/components/round_buttons.dart';
+import 'package:rehab/utils/components/colors.dart';
+import 'package:rehab/utils/utils.dart';
 import 'package:rehab/view_model/firebase_calls.dart';
+
+import '../utils/components/round_buttons.dart';
 
 class PracticePage extends StatefulWidget {
   const PracticePage({super.key});
@@ -17,25 +21,58 @@ class _PracticePageState extends State<PracticePage> {
   @override
   Widget build(BuildContext context) {
     bool loading = false;
-    final databaseRef = FirebaseDatabase.instance.ref('id');
-    print(user?.email);
-    print(user?.uid);
+    var uid = user?.uid;
+    final databaseRef = FirebaseDatabase.instance.ref('id/$uid/sessions');
+    final day = DateTime.now().day;
+    final month = DateTime.now().month;
+    final year = DateTime.now().year;
+    final minute = DateTime.now().minute;
+    final hour = DateTime.now().hour;
+    if (kDebugMode) {
+      print(user?.email);
+      print(user?.uid);
+      print(day);
+      print(month);
+      print(year);
+      print(minute);
+      print(hour);
+    }
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-        ),
-        body:
-            // Column(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     const Center(child: Text("Practice Page")),
-            //     RoundButton(
-            //         title: "Sign Out",
-            //         onPress: () {
-            //           FirebaseCalls().signOut(context);
-            //         })
-            //   ],
-            // ),
-            Container());
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RoundButton(
+              title: "Sign Out",
+              onPress: () {
+                FirebaseCalls().signOut(context);
+              }),
+          SizedBoxWidget.box(50.0),
+          Center(
+            child: RoundButton(
+                title: "Add data",
+                onPress: () {
+                  if (kDebugMode) {
+                    print("Clicked");
+                  }
+                  databaseRef
+                      .child("$day-$month-$year")
+                      .update({'$hour:$minute': "Example"}).then((value) {
+                    Utils.flushBarErrorMessage("Added Session", context,
+                        color: Constants.blueColor);
+                  }).catchError((error, stackTrace) {
+                    if (kDebugMode) {
+                      print(error.toString());
+                    }
+                    Utils.flushBarErrorMessage(
+                        error.message.toString(), context);
+                  });
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
