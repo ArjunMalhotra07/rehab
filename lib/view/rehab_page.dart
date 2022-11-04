80f89aca87309ce74ea347b7ec806a590a5cb332
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-11-05 03:22:32 +0530

    Added asset images

--- lib/view/rehab_page.dart
+++ lib/view/rehab_page.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import 'package:rehab/utils/components/colors.dart';
+import 'package:rehab/utils/components/round_buttons.dart';
 
 class RehabPage extends StatefulWidget {
   const RehabPage({super.key});
@@ -23,22 +25,36 @@ class _RehabPageState extends State<RehabPage> {
       for (final child in event.snapshot.children) {
         // Handle the post.
         var dataObject = child.value.toString();
-        print("inside");
         print(dataObject);
-        print("");
       }
     }, onError: (error) {});
     return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 30.0),
+          child: Text(
+            "Rehab Programme",
+            style: TextStyle(color: Constants.blackColor, fontSize: 30),
+          ),
+        ),
+        SizedBoxWidget.box(10.0),
+        Padding(
+          padding: const EdgeInsets.only(left: 30.0, right: 30),
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset('assets/doctor.png')),
+        ),
         Expanded(
           child: FirebaseAnimatedList(
               query: ref,
               itemBuilder: ((context, snapshot, animation, index) {
                 // var object = (snapshot.children)[index];
 
-                return ListTile(
-                  title: Text(snapshot.children.toString()),
-                );
+                return const ListTile(
+                    // title: Text(snapshot.children.toString()),
+                    );
               })),
         )
       ],
