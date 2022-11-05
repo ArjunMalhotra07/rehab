23368088d122de3d21101d00291a2104f1dac2f7
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-11-05 10:18:50 +0530

    Made UI

--- lib/view/rehab_page.dart
+++ lib/view/rehab_page.dart
@@ -22,42 +22,119 @@ class _RehabPageState extends State<RehabPage> {
     var uid = user?.uid;
     final ref = FirebaseDatabase.instance.ref('uids/$uid/sessions');
     ref.onValue.listen((event) {
-      for (final child in event.snapshot.children) {
-        // Handle the post.
-        var dataObject = child.value.toString();
-        print(dataObject);
+      // for (final child in event.snapshot.children) {
+      //   // Handle the post.
+      //   var dataObject = child.value.toString();
+      //   print(dataObject);
+      // }
+      Map dataObject = {
+        "5-11-2022": {
+          "2:9": "Example728",
+          "3:15": "Example644",
+          "1:45": "Example",
+          "2:11": "Example184",
+          "2:7": "Example",
+        },
+        "6-11-2022": {
+          "2:16": "Example440",
+          "2:26": "Example124",
+          "2:22": "Example515",
+        },
+      };
+
+      for (MapEntry item in dataObject.entries) {
+        String day = item.key.toString();
+        debugPrint("day: $day");
+        for (MapEntry item2 in dataObject[item.key].entries) {
+          String hour = item2.key.toString();
+          String value = item2.value.toString();
+          debugPrint("hour: $hour value: $value");
+        }
       }
     }, onError: (error) {});
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: EdgeInsets.only(left: 30.0),
-          child: Text(
+    return Padding(
+      padding: const EdgeInsets.only(left: 30.0, right: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
             "Rehab Programme",
             style: TextStyle(color: Constants.blackColor, fontSize: 30),
           ),
-        ),
-        SizedBoxWidget.box(10.0),
-        Padding(
-          padding: const EdgeInsets.only(left: 30.0, right: 30),
-          child: ClipRRect(
+          SizedBoxWidget.box(10.0),
+          ClipRRect(
               borderRadius: BorderRadius.circular(8.0),
               child: Image.asset('assets/doctor.png')),
-        ),
-        Expanded(
-          child: FirebaseAnimatedList(
-              query: ref,
-              itemBuilder: ((context, snapshot, animation, index) {
-                // var object = (snapshot.children)[index];
+          SizedBoxWidget.box(10.0),
+          Row(
+            children: [
+              const Text(
+                "History",
+                style: TextStyle(color: Constants.blackColor, fontSize: 22),
+              ),
+              const Spacer(),
+              Image.asset('assets/funnel.png', height: 30, fit: BoxFit.fill),
+            ],
+          ),
+          SizedBoxWidget.box(5.0),
+          Container(
+              height: 85,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: Colors.grey.shade400,
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: const [
+                          Text(
+                            "Total Sessions",
+                            style: TextStyle(
+                                color: Constants.blackColor, fontSize: 15),
+                          ),
+                          Text("16")
+                        ],
+                      ),
+                      SizedBox(width: 5),
+                      Container(
+                        width: 1,
+                        height: 25,
+                        color: Constants.blackColor,
+                      ),
+                      SizedBox(width: 5),
+                      Column(
+                        children: const [
+                          Text(
+                            "Total time",
+                            style: TextStyle(
+                                color: Constants.blackColor, fontSize: 15),
+                          ),
+                          Text("16")
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )),
+          Expanded(
+            child: FirebaseAnimatedList(
+                query: ref,
+                itemBuilder: ((context, snapshot, animation, index) {
+                  // var object = (snapshot.children)[index];
 
-                return const ListTile(
-                    // title: Text(snapshot.children.toString()),
-                    );
-              })),
-        )
-      ],
+                  return const ListTile(
+                      // title: Text(snapshot.children.toString()),
+                      );
+                })),
+          )
+        ],
+      ),
     );
   }
 }
