651dbe399bdf587513bba2fcc73b5f5f9ca80c49
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-11-07 03:48:44 +0530

    removed test page

--- lib/view/practice_screen.dart
+++ lib/view/practice_screen.dart
@@ -1,11 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:rehab/utils/components/round_buttons.dart';
-import 'package:rehab/view/test.dart';
-import 'package:rehab/view_model/getter.dart';
-
-import '../utils/components/colors.dart';
 
 class PracticePage extends StatefulWidget {
   PracticePage({Key? key}) : super(key: key);
@@ -17,19 +11,8 @@ class PracticePage extends StatefulWidget {
 class _PracticePageState extends State<PracticePage> {
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
-    var uid = user?.uid;
-    final controller = Get.put(ListenFirebase1(uid));
     return Center(
-        child: RoundButton(
-      title: "Dummy Page",
-      width: 320,
-      onPress: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => DummyHomePage()),
-        );
-      },
-      buttonColor: Constants.blueColor,
-    ));
+      child: TextStyleWidget.textStyle("Practice Page", 22),
+    );
   }
 }
