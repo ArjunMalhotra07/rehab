eecc42be30f01f5e10eacdf1570a63d1f34abf67
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-11-07 02:51:51 +0530

    Added getx

--- lib/view/practice_screen.dart
+++ lib/view/practice_screen.dart
@@ -1,4 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:rehab/utils/components/round_buttons.dart';
+import 'package:rehab/view/test.dart';
+import 'package:rehab/view_model/getter.dart';
+
+import '../utils/components/colors.dart';
 
 class PracticePage extends StatefulWidget {
   PracticePage({Key? key}) : super(key: key);
@@ -10,6 +17,19 @@ class PracticePage extends StatefulWidget {
 class _PracticePageState extends State<PracticePage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Practice Page"));
+    final user = FirebaseAuth.instance.currentUser;
+    var uid = user?.uid;
+    final controller = Get.put(ListenFirebase1(uid));
+    return Center(
+        child: RoundButton(
+      title: "Dummy Page",
+      width: 320,
+      onPress: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => DummyHomePage()),
+        );
+      },
+      buttonColor: Constants.blueColor,
+    ));
   }
 }
--- lib/view_model/getter.dart
+++ lib/view_model/getter.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
 class ListenFirebase extends ChangeNotifier {
@@ -37,4 +38,32 @@ class ListenFirebase extends ChangeNotifier {
     _keysList.add(value);
     notifyListeners();
   }
+}
+
+class ListenFirebase1 extends GetxController {
+  final user = FirebaseAuth.instance.currentUser;
+  final String? userId;
+  ListenFirebase1(this.userId);
+  final now = DateTime.now();
+  final now2 = DateFormat('hh:mm a').format(DateTime.now());
+
+  RxInt counterVar = 0.obs;
+  int get counter => counterVar.value;
+  setCounter(int value) {
+    counterVar.value = value;
+    update();
+  }
+
+  funcGetTodayEntries(String s) async {
+    int count = 0;
+    final ref = FirebaseDatabase.instance.ref('uids/$userId/sessions/$s');
+    DatabaseEvent event = await ref.once();
+    var object = event.snapshot;
+    for (final _ in object.children) {
+      count += 1;
+      setCounter(count);
+    }
+  }
+
+  int counterTest = 16;
 }
