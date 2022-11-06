aa2a2d2c66c0d0b37974de4692536fe86485b8f8
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-11-07 03:24:52 +0530

    Fixed issue on home page

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -80,51 +80,55 @@ class _HomePageState extends State<HomePage> {
                             Border.all(color: Constants.greyColor, width: 3)),
                     height: 150,
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 7.0, horizontal: 15),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              TextStyleWidget.textStyle("Today's Progress", 23,
-                                  c: Constants.blackShade1),
-                              const Spacer(),
-                              TextStyleWidget.textStyle(
-                                  "${controller.counter * 10}%", 22,
-                                  c: Constants.blueColor),
-                            ],
-                          ),
-                          SizedBoxWidget.box(20.0),
-                          LinearPercentIndicator(
-                            lineHeight: 15,
-                            progressColor: Constants.blueColor,
-                            percent: controller.counter * .1,
-                            animationDuration: 700,
-                            animation: true,
-                            barRadius: const Radius.circular(12),
-                            backgroundColor: Constants.lightGreyColor,
-                          ),
-                          const Spacer(),
-                          Row(
-                            children: [
-                              Image.asset('assets/tick.png',
-                                  height: 50, fit: BoxFit.fill),
-                              TextStyleWidget.textStyle(
-                                  "Completed \n${controller.counter} sessions",
-                                  15,
-                                  c: Constants.blackShade1),
-                              const Spacer(),
-                              Image.asset('assets/arrow.png',
-                                  height: 50, fit: BoxFit.fill),
-                              TextStyleWidget.textStyle(
-                                  " Pending \n${10 - controller.counter} sessions",
-                                  15,
-                                  c: Constants.blackShade1),
-                            ],
-                          ),
-                        ],
-                      ),
-                    )),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 7.0, horizontal: 15),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                TextStyleWidget.textStyle(
+                                    "Today's Progress", 23,
+                                    c: Constants.blackShade1),
+                                const Spacer(),
+                                Obx(
+                                  () => TextStyleWidget.textStyle(
+                                      "${controller.counter * 10}%", 22,
+                                      c: Constants.blueColor),
+                                ),
+                              ],
+                            ),
+                            SizedBoxWidget.box(20.0),
+                            Obx(
+                              () => LinearPercentIndicator(
+                                lineHeight: 15,
+                                progressColor: Constants.blueColor,
+                                percent: controller.counter * .1,
+                                animationDuration: 700,
+                                animation: true,
+                                barRadius: const Radius.circular(12),
+                                backgroundColor: Constants.lightGreyColor,
+                              ),
+                            ),
+                            const Spacer(),
+                            Row(
+                              children: [
+                                Image.asset('assets/tick.png',
+                                    height: 50, fit: BoxFit.fill),
+                                Obx(() => TextStyleWidget.textStyle(
+                                    "Completed \n${controller.counter} sessions",
+                                    15,
+                                    c: Constants.blackShade1)),
+                                const Spacer(),
+                                Image.asset('assets/arrow.png',
+                                    height: 50, fit: BoxFit.fill),
+                                Obx(() => TextStyleWidget.textStyle(
+                                    " Pending \n${10 - controller.counter} sessions",
+                                    15,
+                                    c: Constants.blackShade1)),
+                              ],
+                            ),
+                          ],
+                        ))),
                 SizedBoxWidget.box(35.0),
                 // FirebaseAnimatedList
                 Padding(
@@ -181,35 +185,41 @@ class _HomePageState extends State<HomePage> {
             Positioned(
               top: MediaQuery.of(context).size.height * .68,
               child: Center(
-                  child: RoundButton(
-                title: "Start Session ${controller.counter + 1}",
-                width: 320,
-                onPress: () {
-                  if (controller.counter != 10) {
-                    if (kDebugMode) {
-                      print("Clicked");
-                    }
-                    databaseRef1.child("$day-${now.month}-${now.year}").update({
-                      now2: "Session ${controller.counter + 1}"
-                    }).then((value) {
-                      Utils.flushBarErrorMessage("Added Session", context,
-                          color: Constants.blueColor);
-                      context.read<ListenFirebase>().funcGetTodayEntries(
-                          "${now.day}-${now.month}-${now.year}");
-                    }).catchError((error, stackTrace) {
-                      if (kDebugMode) {
-                        print(error.toString());
-                      }
-                      Utils.flushBarErrorMessage(
-                          error.message.toString(), context);
-                    });
-                  } else {
-                    Utils.flushBarErrorMessage("No sessions for today", context,
-                        color: Constants.redColor);
-                  }
-                },
-                buttonColor: Constants.blueColor,
-              )),
+                  child: Obx((() => RoundButton(
+                        title: "Start Session ${controller.counter + 1}",
+                        width: 320,
+                        onPress: () {
+                          if (controller.counter != 10) {
+                            if (kDebugMode) {
+                              print("Clicked");
+                            }
+                            databaseRef1
+                                .child("$day-${now.month}-${now.year}")
+                                .update({
+                              now2: "Session ${controller.counter + 1}"
+                            }).then((value) {
+                              Utils.flushBarErrorMessage(
+                                  "Added Session", context,
+                                  color: Constants.blueColor);
+                              context
+                                  .read<ListenFirebase>()
+                                  .funcGetTodayEntries(
+                                      "${now.day}-${now.month}-${now.year}");
+                            }).catchError((error, stackTrace) {
+                              if (kDebugMode) {
+                                print(error.toString());
+                              }
+                              Utils.flushBarErrorMessage(
+                                  error.message.toString(), context);
+                            });
+                          } else {
+                            Utils.flushBarErrorMessage(
+                                "No sessions for today", context,
+                                color: Constants.redColor);
+                          }
+                        },
+                        buttonColor: Constants.blueColor,
+                      )))),
             )
           ],
         ),
